52d36f69d0d6fcb036d8a86bb04d84b87fd51587
Carlos Umaña Acevedo <carumtito@gmail.com>
2023-10-03 04:08:05 +0000

    Add ts-node-dev and ts-standard

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import express from 'express'
 
 const app = express()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT ?? 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World Carlos Eduardo V3!')
+	res.send('Hello World Carlos Eduardo')
 })
 
 app.listen(port, () => {
-  console.log('Server is running on port ' + port)
+	console.log(`Server is running on port ${port}`)
 })
