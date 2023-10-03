1aa4e046541d630367be9d1f507ef049e51c768d
Carlos Umaña Acevedo <carumtito@gmail.com>
2023-10-03 00:40:20 +0000

    Add volumes to docker compose dev

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World Carlos Eduardo V2!')
+  res.send('Hello World Carlos Eduardo V3!')
 })
 
 app.listen(port, () => {
