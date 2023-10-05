63d75586ba319a023a104aa5f754d4dd1c963606
Carlos Umaña Acevedo <carumtito@gmail.com>
2023-10-05 05:04:43 +0000

    Add config files

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,11 @@ import { Database } from './database'
 import { Server } from './server'
 import { UserController } from './controllers/users.controller'
 import { usersRoutes } from './routes/users.routes'
+import databaseConfig from '../config/database.config'
 
 const port = process.env.PORT ?? 3000
 
-const database = new Database({
-	host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-})
+const database = new Database(databaseConfig)
 
 const server = new Server(
     [
