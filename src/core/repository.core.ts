c9fff96cad719c7a02dbb17294f445becc07771c
Carlos Umaña Acevedo <carumtito@gmail.com>
2023-10-21 00:20:10 +0000

    Add more methods to repository core

--- src/core/repository.core.ts
+++ src/core/repository.core.ts
@@ -1,29 +1,50 @@
+import { generateCode } from '@/utils/code'
 import { PaginationSchemaType, ResponsePaginationSchemaType } from '@/utils/pagination'
 import { SQL, and, isNull, sql } from 'drizzle-orm'
-import { MySqlColumn, MySqlTable } from 'drizzle-orm/mysql-core'
+import { MySqlTable } from 'drizzle-orm/mysql-core'
 import { MySql2Database } from 'drizzle-orm/mysql2'
+
+type Where = SQL<unknown> | undefined
 
 interface RepositoryCoreParams {
     db: MySql2Database
     table: MySqlTable
-    deletedColumn: MySqlColumn
+    select: any
+}
+
+interface PaginateParams {
+    query: PaginationSchemaType
+    where: Where
+}
+
+interface SelectorParams {
+    where: Where
+    per_page?: number
+    offset?: number
+}
+
+interface InsertParams<E> {
+    params: E
+}
+
+interface UpdateParams<E> {
+    where: Where
+    params: Partial<E>
 }
 
 export class RepositoryCore<T> {
-    public readonly db: MySql2Database
-    public readonly table: MySqlTable
-    public readonly deletedColumn: MySqlColumn
+    protected readonly db: MySql2Database
+    protected readonly table: MySqlTable
+    protected readonly select: any
 
-    constructor ({ db, table, deletedColumn }: RepositoryCoreParams) {
-        this.db = db
-        this.table = table
-        this.deletedColumn = deletedColumn
+    constructor (data: RepositoryCoreParams) {
+        this.db = data.db
+        this.table = data.table
+        this.select = data.select
     }
 
-    public async selector (where: SQL<unknown> | undefined, per_page?: number, offset?: number): Promise<T[]> {
-        let query = this.db.select()
-            .from(this.table)
-            .where(and(where, isNull(this.deletedColumn)))
+    protected async selector ({ where, offset, per_page }: SelectorParams): Promise<T[]> {
+        let query = this.select.where(and(where, isNull(sql`deleted_at`)))
 
         if (per_page !== undefined) {
             query = query.limit(per_page)
@@ -36,12 +57,12 @@ export class RepositoryCore<T> {
         return await query as T[]
     }
 
-    public async paginate (where: SQL<unknown> | undefined, query: PaginationSchemaType): Promise<ResponsePaginationSchemaType<T>> {
+    protected async paginate ({ query, where }: PaginateParams): Promise<ResponsePaginationSchemaType<T>> {
         const { page, per_page } = query
 
         const offset = (page - 1) * per_page
 
-        const data = await this.selector(where, per_page, offset)
+        const data = await this.selector({ where, per_page, offset })
         const total = await this.count(where)
 
         return {
@@ -55,19 +76,38 @@ export class RepositoryCore<T> {
         }
     }
 
-    public async count (where: SQL<unknown> | undefined): Promise<number> {
-        const total = await this.db.select({ count: sql<number>`count(id)` })
-            .from(this.table)
-            .where(and(where, isNull(this.deletedColumn)))
-
-        return total[0].count
-    }
-
-    public async softDelete (where: SQL<unknown>): Promise<boolean> {
+    protected async set<E> ({ where, params }: UpdateParams<E>): Promise<boolean> {
         const data = await this.db.update(this.table)
-            .set({ deleted_at: sql`CURRENT_TIMESTAMP` })
-            .where(and(where, isNull(this.deletedColumn)))
+            .set(params)
+            .where(and(where, isNull(sql`deleted_at`)))
 
         return data[0].affectedRows > 0
+    }
+
+    protected async insert<E> ({ params }: InsertParams<E>): Promise<string> {
+        const code = generateCode()
+
+        await this.db.insert(this.table).values({
+            ...params,
+            code
+        })
+
+        return code
+    }
+
+    protected async softDelete (where: Where): Promise<boolean> {
+        const data = await this.db.update(this.table)
+            .set({ deleted_at: sql`CURRENT_TIMESTAMP` })
+            .where(and(where, isNull(sql`deleted_at`)))
+
+        return data[0].affectedRows > 0
+    }
+
+    protected async count (where: Where): Promise<number> {
+        const total = await this.db.select({ count: sql<number>`count(id)` })
+            .from(this.table)
+            .where(and(where, isNull(sql`deleted_at`)))
+
+        return total[0].count
     }
 }
--- src/repositories/companies_modality.repository.ts
+++ src/repositories/companies_modality.repository.ts
@@ -1,61 +1,62 @@
 import { MySql2Database } from 'drizzle-orm/mysql2'
-import { eq, and, isNull } from 'drizzle-orm'
+import { eq } from 'drizzle-orm'
 import { ICompanyModalityRepository } from './repositories'
 import { CompanyModalitySchemaCreateType, CompanyModalitySchemaSelect, CompanyModalitySchemaSelectPaginated, CompanyModalitySchemaSelectPaginatedType, CompanyModalitySchemaSelectType, CompanyModalitySchemaUpdateType, companies_modality } from '@models/companies_modality.model'
-import { generateCode } from '@utils/code'
 import { PaginationSchemaType } from '@/utils/pagination'
 import { RepositoryCore } from '@/core/repository.core'
 
 export class CompaniesModalityRepository extends RepositoryCore<CompanyModalitySchemaSelectType> implements ICompanyModalityRepository {
     constructor (public readonly db: MySql2Database) {
-        super({
-            db,
-            table: companies_modality,
-            deletedColumn: companies_modality.deleted_at
+        const table = companies_modality
+
+        const select = db.select({
+            code: companies_modality.code,
+            name: companies_modality.name
         })
+        .from(table)
+
+        super({ db, table, select })
     }
 
     public async getAll (group_id: number, query: PaginationSchemaType): Promise<CompanyModalitySchemaSelectPaginatedType> {
-        const data = await this.paginate(eq(companies_modality.group_id, group_id), query)
+        const data = await this.paginate({
+            query,
+            where: eq(companies_modality.group_id, group_id)
+        })
 
         return CompanyModalitySchemaSelectPaginated.parse(data)
     }
 
     public async get (code: string): Promise<CompanyModalitySchemaSelectType | null> {
-        const data = await this.selector(eq(companies_modality.code, code))
+        const data = await this.selector({
+            where: eq(companies_modality.code, code)
+        })
 
-        return data.length > 0
-            ? CompanyModalitySchemaSelect.parse(data.at(0))
-            : null
+        if (data.length === 0) {
+            return null
+        }
+
+        return CompanyModalitySchemaSelect.parse(data.at(0))
     }
 
     public async create (params: CompanyModalitySchemaCreateType): Promise<string> {
-        const code = generateCode()
-
-        await this.db.insert(companies_modality).values({
-            ...params,
-            code
+        const code = await this.insert<CompanyModalitySchemaCreateType>({
+            params
         })
 
         return code
     }
 
     public async update (code: string, params: CompanyModalitySchemaUpdateType): Promise<string> {
-        const data = await this.db.update(companies_modality)
-            .set(params)
-            .where(
-                and(
-                    eq(companies_modality.code, code),
-                    isNull(companies_modality.deleted_at)
-                )
-            )
+        const data = await this.set<CompanyModalitySchemaUpdateType>({
+            params,
+            where: eq(companies_modality.code, code)
+        })
 
-        return data[0].affectedRows > 0 ? code : ''
+        return data ? code : ''
     }
 
     public async delete (code: string): Promise<boolean> {
-        const where = eq(companies_modality.code, code)
-
-        return await this.softDelete(where)
+        return await this.softDelete(eq(companies_modality.code, code))
     }
 }
