971bf6ad98a1ff8723223ab178a78abfaa9781ff
Carlos Umaña Acevedo <carumtito@gmail.com>
2024-02-08 02:52:08 +0000

    Update token and refresh token expiration times

--- config/jwt.config.ts
+++ config/jwt.config.ts
@@ -1,10 +1,10 @@
 export default {
     token: {
-        expiresIn: 1200, // 20 minutes
+        expiresIn: 32400, // 9 hours
         secret: process.env.SECRET_TOKEN ?? 'secret_token'
     },
     refreshToken: {
-        expiresIn: 3600, // 60 minutes
+        expiresIn: 86400, // 24 hours
         secret: process.env.SECRET_REFRESH_TOKEN ?? 'secret_refresh_token'
     }
 }
